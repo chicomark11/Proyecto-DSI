06db358301bc9bf4d30b2dc5d4674bde29e318a7
chicomark11 <maiespin@alumnos.ubiobio.cl>
2017-06-25 18:54:59 -0400

    Archivos
    Basic

---
+++ basic/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=orecal',
+    'username' =>''
+    'password' => 'master9sword3',
+    'charset' => 'utf8',
+];
